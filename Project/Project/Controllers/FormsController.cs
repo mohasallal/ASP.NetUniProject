c6cdab656d4ebe4ea3a7d8a7c5474e906e8fff65
Mohammad Alsallal <malslal787@gmail.com>
2024-04-14 18:26:20 +0300

    completingTheAssigment
    added Forms from 7 - form 11

--- Project/Project/Controllers/FormsController.cs
+++ Project/Project/Controllers/FormsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
+
 namespace Project.Controllers
 {
     public class FormsController : Controller
@@ -111,6 +113,19 @@ namespace Project.Controllers
         [HttpPost]
         public IActionResult Form4(string name,string password)
         {
+            string result;
+
+            if(name == "ASP" && password == "ASP")
+            {
+                ViewBag.result = "Valid User";
+            }
+            else
+            {
+                ViewBag.result = "Invalid User";
+            }
+
+            ViewBag.name = name;
+            ViewBag.password = password;
 
             return View();
         }
@@ -152,6 +167,96 @@ namespace Project.Controllers
         {
             return RedirectToAction("Form5");
         }
+
+        [HttpGet]
+        public IActionResult Form7()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form7(string productName, double price , int Count)
+        {
+            double Total = price * Count;
+
+
+            TempData["Total"] = Total.ToString();
+            TempData["productName"] = productName.ToString();
+            TempData["price"] = price.ToString();
+            TempData["Count"] = Count.ToString();
+
+            return RedirectToAction("Form8");
+        }
+
+        [HttpGet]
+        public IActionResult Form8()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Form8")]
+        public IActionResult BackToForm7()
+        {
+            return RedirectToAction("Form7");
+        }
+
+        [HttpGet]
+        public IActionResult Form9()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form9(string userName,string productName)
+        {
+            HttpContext.Session.SetString("userName" , userName.ToString());
+            HttpContext.Session.SetString("productName", productName.ToString());
+
+            return RedirectToAction("Form10");
+        }
+
+        [HttpGet]
+        public IActionResult Form10()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form10(double price,int count)
+        {   
+            double total = price * count;
+
+            HttpContext.Session.SetString("total", total.ToString());
+            HttpContext.Session.SetString("price", price.ToString());
+            HttpContext.Session.SetString("count", count.ToString());
+
+
+
+            return RedirectToAction("Form11");
+        }
+
+        [HttpGet]
+        public IActionResult Form11()
+        {
+
+            ViewBag.userName =  HttpContext.Session.GetString("userName");
+            ViewBag.productName = HttpContext.Session.GetString("productName");
+            ViewBag.total = HttpContext.Session.GetString("total");
+            ViewBag.price = HttpContext.Session.GetString("price");
+            ViewBag.count = HttpContext.Session.GetString("count");
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName("Form11")]
+        public IActionResult BacktoForm9()
+        {
+            return RedirectToAction("Form9");
+        }
+
 
     }
 }
